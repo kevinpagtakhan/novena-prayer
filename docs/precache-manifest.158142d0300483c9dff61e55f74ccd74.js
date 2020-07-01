bbd7c087640aa881978bf7a8eaa12c0382528de8
Kevin Pagtakhan <kevpagtakhan@gmail.com>
2020-07-01 03:03:12 +0000

    Generated build files

---
+++ docs/precache-manifest.158142d0300483c9dff61e55f74ccd74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d217421f1351a724ad9b214170e5dac",
+    "url": "/novena-prayer/index.html"
+  },
+  {
+    "revision": "4be5d92f526d03a4621e",
+    "url": "/novena-prayer/static/css/main.9ebb3aa7.chunk.css"
+  },
+  {
+    "revision": "43606f56639795c1fe22",
+    "url": "/novena-prayer/static/js/2.32b57e5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/novena-prayer/static/js/2.32b57e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be5d92f526d03a4621e",
+    "url": "/novena-prayer/static/js/main.0c731e36.chunk.js"
+  },
+  {
+    "revision": "0614ebb3fb628d72ccf9",
+    "url": "/novena-prayer/static/js/runtime-main.e296130c.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.845fb5247a37dc195045d9088ab6f4e8.js"
+  "/novena-prayer/precache-manifest.158142d0300483c9dff61e55f74ccd74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/novena-prayer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
